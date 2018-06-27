7d542debb5e162fd1e7f27823a14251a8ad7f24e
Andrei Oprea <andrei.br92@gmail.com>
2018-06-27 23:34:06 +0200

    first pass

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-import { greet } from "./wasm_game_of_life";
+import { Universe } from "./wasm_game_of_life";
 
-greet("Rust and WebAssembly");
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+const renderLoop = () => {
+    pre.textContent = universe.render();
+    universe.tick();
+
+    requestAnimationFrame(renderLoop);
+}
+
+requestAnimationFrame(renderLoop);
--- wasm_game_of_life.js
+++ wasm_game_of_life.js
@@ -1,30 +1,22 @@
 /* tslint:disable */
 import * as wasm from './wasm_game_of_life_bg';
 
-const TextDecoder = typeof window === 'object' && window.TextDecoder
-    ? window.TextDecoder
+const TextDecoder = typeof self === 'object' && self.TextDecoder
+    ? self.TextDecoder
     : require('util').TextDecoder;
 
 let cachedDecoder = new TextDecoder('utf-8');
 
-let cachedUint8Memory = null;
+let cachegetUint8Memory = null;
 function getUint8Memory() {
-    if (cachedUint8Memory === null ||
-        cachedUint8Memory.buffer !== wasm.memory.buffer)
-        cachedUint8Memory = new Uint8Array(wasm.memory.buffer);
-    return cachedUint8Memory;
+    if (cachegetUint8Memory === null ||
+        cachegetUint8Memory.buffer !== wasm.memory.buffer)
+        cachegetUint8Memory = new Uint8Array(wasm.memory.buffer);
+    return cachegetUint8Memory;
 }
 
 function getStringFromWasm(ptr, len) {
-    return cachedDecoder.decode(getUint8Memory().slice(ptr, ptr + len));
-}
-
-let cachedUint32Memory = null;
-function getUint32Memory() {
-    if (cachedUint32Memory === null ||
-        cachedUint32Memory.buffer !== wasm.memory.buffer)
-        cachedUint32Memory = new Uint32Array(wasm.memory.buffer);
-    return cachedUint32Memory;
+    return cachedDecoder.decode(getUint8Memory().subarray(ptr, ptr + len));
 }
 
 let cachedGlobalArgumentPtr = null;
@@ -34,19 +26,160 @@ function globalArgumentPtr() {
     return cachedGlobalArgumentPtr;
 }
 
-function getGlobalArgument(arg) {
-    const idx = globalArgumentPtr() / 4 + arg;
-    return getUint32Memory()[idx];
+let cachegetUint32Memory = null;
+function getUint32Memory() {
+    if (cachegetUint32Memory === null ||
+        cachegetUint32Memory.buffer !== wasm.memory.buffer)
+        cachegetUint32Memory = new Uint32Array(wasm.memory.buffer);
+    return cachegetUint32Memory;
 }
 
-export function __wbg_f_alert_alert_n(arg0) {
-    let len0 = getGlobalArgument(0);
-    let v0 = getStringFromWasm(arg0, len0);
-    alert(v0);
+export class Universe {
+
+                static __construct(ptr) {
+                    return new Universe(ptr);
+                }
+
+                constructor(ptr) {
+                    this.ptr = ptr;
+                }
+
+            free() {
+                const ptr = this.ptr;
+                this.ptr = 0;
+                wasm.__wbg_universe_free(ptr);
+            }
+        static new() {
+    return Universe.__construct(wasm.universe_new());
+}
+tick() {
+    return wasm.universe_tick(this.ptr);
+}
+render() {
+    const retptr = globalArgumentPtr();
+    wasm.universe_render(retptr, this.ptr);
+    const mem = getUint32Memory();
+    const ptr = mem[retptr / 4];
+    const len = mem[retptr / 4 + 1];
+    const realRet = getStringFromWasm(ptr, len).slice();
+    wasm.__wbindgen_free(ptr, len * 1);
+    return realRet;
+}
 }
 
-const TextEncoder = typeof window === 'object' && window.TextEncoder
-    ? window.TextEncoder
+let slab = [];
+
+let slab_next = 0;
+
+function addHeapObject(obj) {
+    if (slab_next === slab.length)
+        slab.push(slab.length + 1);
+    const idx = slab_next;
+    const next = slab[idx];
+
+    slab_next = next;
+
+    slab[idx] = { obj, cnt: 1 };
+    return idx << 1;
+}
+
+let stack = [];
+
+function getObject(idx) {
+    if ((idx & 1) === 1) {
+        return stack[idx >> 1];
+    } else {
+        const val = slab[idx >> 1];
+
+    return val.obj;
+
+    }
+}
+
+export function __wbindgen_object_clone_ref(idx) {
+    // If this object is on the stack promote it to the heap.
+    if ((idx & 1) === 1)
+        return addHeapObject(getObject(idx));
+
+    // Otherwise if the object is on the heap just bump the
+    // refcount and move on
+    const val = slab[idx >> 1];
+    val.cnt += 1;
+    return idx;
+}
+
+function dropRef(idx) {
+
+    let obj = slab[idx >> 1];
+
+    obj.cnt -= 1;
+    if (obj.cnt > 0)
+        return;
+
+    // If we hit 0 then free up our space in the slab
+    slab[idx >> 1] = slab_next;
+    slab_next = idx >> 1;
+}
+
+export function __wbindgen_object_drop_ref(i) { dropRef(i); }
+
+export function __wbindgen_string_new(p, l) {
+    return addHeapObject(getStringFromWasm(p, l));
+}
+
+export function __wbindgen_number_new(i) { return addHeapObject(i); }
+
+export function __wbindgen_number_get(n, invalid) {
+    let obj = getObject(n);
+    if (typeof(obj) === 'number')
+        return obj;
+    getUint8Memory()[invalid] = 1;
+    return 0;
+}
+
+export function __wbindgen_undefined_new() { return addHeapObject(undefined); }
+
+export function __wbindgen_null_new() {
+    return addHeapObject(null);
+}
+
+export function __wbindgen_is_null(idx) {
+    return getObject(idx) === null ? 1 : 0;
+}
+
+export function __wbindgen_is_undefined(idx) {
+    return getObject(idx) === undefined ? 1 : 0;
+}
+
+export function __wbindgen_boolean_new(v) {
+    return addHeapObject(v === 1);
+}
+
+export function __wbindgen_boolean_get(i) {
+    let v = getObject(i);
+    if (typeof(v) === 'boolean') {
+        return v ? 1 : 0;
+    } else {
+        return 2;
+    }
+}
+
+export function __wbindgen_symbol_new(ptr, len) {
+    let a;
+    if (ptr === 0) {
+        a = Symbol();
+    } else {
+        a = Symbol(getStringFromWasm(ptr, len));
+    }
+    return addHeapObject(a);
+}
+
+export function __wbindgen_is_symbol(i) {
+    return typeof(getObject(i)) === 'symbol' ? 1 : 0;
+}
+
+const TextEncoder = typeof self === 'object' && self.TextEncoder
+    ? self.TextEncoder
     : require('util').TextEncoder;
 
 let cachedEncoder = new TextEncoder('utf-8');
@@ -59,19 +192,13 @@ function passStringToWasm(arg) {
     return [ptr, buf.length];
 }
 
-function setGlobalArgument(arg, i) {
-    const idx = globalArgumentPtr() / 4 + i;
-    getUint32Memory()[idx] = arg;
-}
-
-export function greet(arg0) {
-    const [ptr0, len0] = passStringToWasm(arg0);
-    setGlobalArgument(len0, 0);
-    try {
-        return wasm.greet(ptr0);
-    } finally {
-        wasm.__wbindgen_free(ptr0, len0 * 1);
-    }
+export function __wbindgen_string_get(i, len_ptr) {
+    let obj = getObject(i);
+    if (typeof(obj) !== 'string')
+        return 0;
+    const [ptr, len] = passStringToWasm(obj);
+    getUint32Memory()[len_ptr / 4] = len;
+    return ptr;
 }
 
 export function __wbindgen_throw(ptr, len) {
